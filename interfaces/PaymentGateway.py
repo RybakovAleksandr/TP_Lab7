e9e6789db638d62321cc3dc1b2a477d12ed02a55
RybakovAleksandr <alexander.rybakov1234@gmail.com>
2025-12-29 17:22:19 +0300

    PymentGateway fix

--- interfaces/PaymentGateway.py
+++ interfaces/PaymentGateway.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from domain.Money import Money
 
 
-def PaymentGateway(ABC):
+class PaymentGateway(ABC):
     @abstractmethod
     def charge(self, orderId: str, amount: Money) -> bool:
         pass
